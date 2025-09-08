c7409aac5102c660c226bee1ca3fa31c46a55464
희은 <datakubit2@gmail.com>
2025-09-08 20:50:52 +0900

    Feat: 프로젝트 설정

---
+++ src/main/java/com/rabbittick/streamer/MarketIngestStreamerApplication.java
@@ -0,0 +1,13 @@
+package com.rabbittick.streamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketIngestStreamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketIngestStreamerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabbittick/streamer/MarketIngestStreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbittick.streamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketIngestStreamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
